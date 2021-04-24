120f43be584c2f41f07517bf4d24c71e139f817b
wntngns23 <wntngns23@gmail.com>
2021-04-24 17:07:15 +0900

    GettingTheWeaterPartOne

--- ChromeAppJS/readme.md
+++ ChromeAppJS/readme.md
@@ -4,3 +4,6 @@ https://nomadcoders.co/javascript-for-beginners/lectures/1475
 
 
 https://flatuicolors.com
+
+//여기서 키를 만들고 weather.js API_KEY에 값을 넣는다.
+https://home.openweathermap.org/api_keys
--- ChromeAppJS/weather.js
+++ ChromeAppJS/weather.js
@@ -1,7 +1,23 @@
 const COORDS = 'coords'
 
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
 function handleGeoSucces(position){
-    console.log(position);
+    //console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    // const coordsObj = {
+    //     latitude:latitude,
+    //     longitude:longitude
+    // }
+    //same
+    const coordsObj = {
+        latitude,
+        longitude
+    }
+    saveCoords(coordsObj);
 }
 
 function handleGeoErrors(){
